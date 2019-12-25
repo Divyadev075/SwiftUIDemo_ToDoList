c7a69691f296b9d609155e9861ff05bcb7369e1e
Divya <matdd974@gmail.com>
2019-12-25 17:54:16 +0530

    Initial Commit

---
+++ SwiftUiTODO_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiTODO_Demo
+//
+//  Created by Divya on 25/12/19.
+//  Copyright © 2019 Divya . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
