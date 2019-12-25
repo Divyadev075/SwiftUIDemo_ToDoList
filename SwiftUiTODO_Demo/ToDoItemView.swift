6784f334109938c96bdcba68288a4b385471adc9
Divya <matdd974@gmail.com>
2019-12-25 18:04:50 +0530

    Initial Commit

---
+++ SwiftUiTODO_Demo/ToDoItemView.swift
@@ -0,0 +1,39 @@
+//
+//  ToDoItemView.swift
+//  SwiftUiTODO_Demo
+//
+//  Created by Divya on 25/12/19.
+//  Copyright © 2019 Divya . All rights reserved.
+//
+
+import SwiftUI
+
+struct ToDoItemView: View {
+    var title:String = ""
+    var createdAt:String = ""
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(title)
+                .font(.headline)
+                Text(createdAt)
+                .font(.caption)
+               
+               
+            }
+            
+
+           
+        }
+    }
+}
+
+
+#if DEBUG
+struct ToDoItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoItemView()
+    }
+}
+#endif
